9e7e79178c4da1e2f46f2da54cbff28de8410520
Darryl Murray <darrylmurray@me.com>
2016-10-17 01:50:23 -0400

    Added more documentation to ImgDownload.

--- app/src/main/java/ca/uwo/eng/se3313/lab2/ImgDownload.java
+++ app/src/main/java/ca/uwo/eng/se3313/lab2/ImgDownload.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.util.LruCache;
 
@@ -19,9 +20,7 @@ import java.net.URL;
  * @author Darryl Murray (dmurra47@uwo.ca)
  * @version 1.0
  */
-public class ImgDownload implements IImageDownloader{
-    // Caching link to check on: https://developer.android.com/training/displaying-bitmaps/cache-bitmap.html
-
+public class ImgDownload implements IImageDownloader {
     private static final int TIMEOUT_SECS = 2;
     private static final int CACHE_SIZE_MiB = 6;
     private ErrorHandler errorHandler;
@@ -36,12 +35,16 @@ public class ImgDownload implements IImageDownloader{
     public ImgDownload(ErrorHandler handler) {
         errorHandler = handler;
         bitmapCache = new LruCache<String, Bitmap>(CACHE_SIZE_MiB * 1024 * 1024){
+            /**
+             * Removes cached bitmaps from memory when removed from the cache.
+             *
+             * @param evicted  true if the entry is being removed to make space, false if the removal was caused by a put(K, V) or remove(K).
+             * @param key      key the entry was stored under.
+             * @param oldValue the bitmap stored.
+             * @param newValue the new value for key, if it exists. If non-null, this removal was caused by a put(K, V). Otherwise it was caused by an eviction or a remove(K).
+             */
             @Override
-            public void entryRemoved(boolean evicted,
-                                     String key,
-                                     Bitmap oldValue,
-                                     Bitmap newValue)
-            {
+            public void entryRemoved(boolean evicted, String key, Bitmap oldValue, Bitmap newValue) {
                 oldValue.recycle();
             }
         };
@@ -57,13 +60,23 @@ public class ImgDownload implements IImageDownloader{
     @Override
     public void download(@NonNull String imageUrl, @NonNull IImageDownloader.SuccessHandler handler) {
         class DownloadTask extends AsyncTask<URL, Void, Bitmap> {
-
             private SuccessHandler handler;
 
+            /**
+             * The constructor for DownloadTask.
+             *
+             * @param handler The {@link ca.uwo.eng.se3313.lab2.IImageDownloader.SuccessHandler} to use on download success.
+             */
             private DownloadTask(SuccessHandler handler) {
                 this.handler = handler;
             }
 
+            /**
+             * Downloads new image in background.
+             *
+             * @param params The URL to download the image from.
+             * @return The bitmap of the downloaded image.
+             */
             @Override
             protected Bitmap doInBackground(URL... params) {
 
@@ -82,8 +95,13 @@ public class ImgDownload implements IImageDownloader{
                 }
             }
 
+            /**
+             * Run after download is complete. Runs the success or error handlers on completion/error.
+             *
+             * @param res The bitmap downloaded. Can be null.
+             */
             @Override
-            protected void onPostExecute(Bitmap res) {
+            protected void onPostExecute(@Nullable Bitmap res) {
                 if (res != null) {
                     handler.onComplete(res);
                 } else {
@@ -96,15 +114,18 @@ public class ImgDownload implements IImageDownloader{
         // Check cache first
         Bitmap cacheResult = bitmapCache.get(imageUrl);
 
-        // If cache didn't have it
+        // Download if cache didn't have it, otherwise return cached result.
         if (cacheResult == null) {
             try {
+                // Create URL object, modify the handler to use in download task, and run task.
                 URL imageURL = new URL(imageUrl);
                 Log.d("onDownload", "cache miss");
+
                 SuccessHandler newHandler = (Bitmap v) -> {
                     bitmapCache.put(imageUrl, v);
                     handler.onComplete(v);
                 };
+
                 new DownloadTask(newHandler).execute(imageURL);
             } catch (MalformedURLException e) {
                 throw new IllegalArgumentException(e.getMessage(), e.getCause());
